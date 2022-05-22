7a2fc599e729cda327b23257ad86a0e5e9246ab6
yddk <56341954+yddk@users.noreply.github.com>
2022-05-22 23:02:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -86,7 +86,7 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/master/luci-app-udpxy p
 # NAS
 
 #文件助手
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
 
 ##############################################################################################
 
